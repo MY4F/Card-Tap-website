3ed99536e6ed703cb4a50911cdd8afc155d05828
MY4F <67027767+MY4F@users.noreply.github.com>
2021-02-16 06:08:05 +0200

    finished responsive desktop and mobile

--- js.js
+++ js.js
@@ -1,6 +1,8 @@
 const navButtton = document.querySelector(".nav-button");
 const list=document.querySelector("ul");
 const listItems= document.querySelectorAll('li');
+const desktopContainer=document.querySelector('.desktop-container');
+const bodyy=document.querySelector('body');
 navButtton.addEventListener('click',()=>{
   if(list.style.display==='none'|| list.style.display===''){
     list.style.display='flex';
@@ -33,5 +35,16 @@ listItems[2].addEventListener('click',()=>{
   }
   else{
     list.style.display='';
+  }
+});
+console.log(desktopContainer.innerHTML);
+window.addEventListener('resize',()=>{
+  console.log(window.innerWidth);
+  if(window.innerWidth<751){
+    desktopContainer.classList.remove("desktop-container");
+
+  }
+  else{
+    desktopContainer.classList.add("desktop-container");
   }
 });
