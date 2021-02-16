65ec5c63fff8f1f6da7c717a9038df66c234d70a
MY4F <67027767+MY4F@users.noreply.github.com>
2021-02-16 04:34:08 +0200

    some responsive in both

--- js.js
+++ js.js
@@ -1,1 +1,37 @@
-f
+const navButtton = document.querySelector(".nav-button");
+const list=document.querySelector("ul");
+const listItems= document.querySelectorAll('li');
+navButtton.addEventListener('click',()=>{
+  if(list.style.display==='none'|| list.style.display===''){
+    list.style.display='flex';
+  }
+  else{
+    list.style.display='';
+  }
+});
+listItems[0].addEventListener('click',()=>{
+  if(list.style.display==='none'|| list.style.display===''){
+    list.style.display='flex';
+  }
+  else{
+    list.style.display='';
+  }
+});
+
+listItems[1].addEventListener('click',()=>{
+  if(list.style.display==='none'|| list.style.display===''){
+    list.style.display='flex';
+  }
+  else{
+    list.style.display='';
+  }
+});
+
+listItems[2].addEventListener('click',()=>{
+  if(list.style.display==='none'|| list.style.display===''){
+    list.style.display='flex';
+  }
+  else{
+    list.style.display='';
+  }
+});
