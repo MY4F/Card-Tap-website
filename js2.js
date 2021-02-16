6416af3bdeb91eba6d3a23c5045ea541a5dd3b50
MY4F <67027767+MY4F@users.noreply.github.com>
2021-02-16 21:22:46 +0200

    trying mobile version on main

---
+++ js2.js
@@ -0,0 +1,39 @@
+const navButtton = document.querySelector(".nav-button");
+const list=document.querySelector("ul");
+const listItems= document.querySelectorAll('li');
+const desktopContainer=document.querySelector('.desktop-container');
+const bodyy=document.querySelector('body');
+navButtton.addEventListener('click',()=>{
+  if(list.style.display==='none'|| list.style.display===''){
+    list.style.display='flex';
+  }
+  else{
+    list.style.display='';
+  }
+});
+listItems[0].addEventListener('click',()=>{
+  if(list.style.display==='none'|| list.style.display===''){
+    list.style.display='flex';
+  }
+  else{
+    list.style.display='';
+  }
+});
+
+listItems[1].addEventListener('click',()=>{
+  if(list.style.display==='none'|| list.style.display===''){
+    list.style.display='flex';
+  }
+  else{
+    list.style.display='';
+  }
+});
+
+listItems[2].addEventListener('click',()=>{
+  if(list.style.display==='none'|| list.style.display===''){
+    list.style.display='flex';
+  }
+  else{
+    list.style.display='';
+  }
+});
