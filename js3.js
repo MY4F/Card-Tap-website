1ff14d7d3c6972284fa02683d2d1e87261b24d07
MY4F <67027767+MY4F@users.noreply.github.com>
2021-02-22 01:29:56 +0200

    finished the package page

---
+++ js3.js
@@ -0,0 +1,15 @@
+const posterClick=document.querySelectorAll('.poster');
+
+
+posterClick[0].addEventListener('click',()=>{
+  window.scrollTo(0,734);
+});
+posterClick[1].addEventListener('click',()=>{
+  window.scrollTo(0,1650);
+});
+posterClick[2].addEventListener('click',()=>{
+  window.scrollTo(0,2560);
+});
+posterClick[3].addEventListener('click',()=>{
+  window.scrollTo(0,3490);
+});
