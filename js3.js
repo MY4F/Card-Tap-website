22faa9dbbad8d6fc1b10522c9af177b0ce0a64ca
MY4F <67027767+MY4F@users.noreply.github.com>
2021-02-22 01:40:30 +0200

    Update js3.js

--- js3.js
+++ js3.js
@@ -11,5 +11,17 @@ posterClick[2].addEventListener('click',()=>{
   window.scrollTo(0,2560);
 });
 posterClick[3].addEventListener('click',()=>{
-  window.scrollTo(0,3490);
+  window.scrollTo(0,3460);
+});
+posterClick[4].addEventListener('click',()=>{
+  window.scrollTo(0,785);
+});
+posterClick[5].addEventListener('click',()=>{
+  window.scrollTo(0,1930);
+});
+posterClick[6].addEventListener('click',()=>{
+  window.scrollTo(0,3085);
+});
+posterClick[7].addEventListener('click',()=>{
+  window.scrollTo(0,4235);
 });
