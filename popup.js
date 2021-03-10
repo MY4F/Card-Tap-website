e5f1c256887d7a0bb0cca5e33e662034713ab410
MY4F <67027767+MY4F@users.noreply.github.com>
2021-03-10 06:52:11 +0200

    test

--- popup.js
+++ popup.js
@@ -1,9 +1,11 @@
 const purchaseBut=document.querySelectorAll('.package-request');
 const popupPurchase= document.querySelector('.popup-purchase');
 const closeSpan=document.querySelector('.close');
+const requestedText=document.querySelector('.requested-type');
 
 purchaseBut[0].addEventListener('click',()=>{
   popupPurchase.style.display='block';
+  requestedText.value="Individuals";
 })
 closeSpan.addEventListener('click',()=>{
   popupPurchase.style.display = "none";
